069971120dd9f83d81a5a504390b8d8a5b1836e6
weihua.liu <weihua1986@gmail.com>
2017-05-02 23:15:04 +0800

    modify

--- store/parse.go
+++ store/parse.go
@@ -28,7 +28,8 @@ func Parse(data []byte) {
 			tag2 := util.BytesToString(pack.TagList[1].Name)
 			tag3 := util.BytesToString(pack.TagList[2].Name)
 			basepath := filepath.Join("data/api", tag1, tag2, util.BytesToString(pack.Date))
-			log.Println(basepath)
+			//log.Println(basepath)
+
 			err = os.MkdirAll(basepath, os.ModePerm)
 			//创建存储目录
 			if err != nil {
@@ -37,11 +38,16 @@ func Parse(data []byte) {
 			filePath := filepath.Join(basepath, tag3)
 			//写入文件
 			strtime := "------------------" + time.Now().Truncate(time.Second).String() + "----------------------"
+			// data write memory
+			monitors.Write(filepath.Join(tag1,tag2,util.BytesToString(pack.Date),tag3), []byte(strtime))
+			// data write local storage
 			err = util.AppendToFile(filePath, []byte(strtime))
 			if err != nil {
 				log.Println(err)
 			}
-
+			// data write memory
+			monitors.Write(filepath.Join(tag1,tag2,util.BytesToString(pack.Date),tag3), pack.Body)
+			// data write local storage
 			err = util.AppendToFile(filePath, pack.Body)
 			if err != nil {
 				log.Println(err)
